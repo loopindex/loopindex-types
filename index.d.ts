a188a37a83dc783b4abed50cf402bc55b4e25b70
dfl <dfl@loopindex.com>
2025-04-16 02:55:04 +0300

    better globals

--- index.d.ts
+++ index.d.ts
@@ -2,13 +2,15 @@ import type { IFLITEGlobals } from "./flite";
 import type { ILanceGlobals } from "./lance";
 
 export type * from "./common";
+
+export interface ILoopIndexGlobals {
+	readonly $: JQueryStatic;
+	readonly FLITE: IFLITEGlobals;
+	readonly LANCE: ILanceGlobals;				
+}
 declare global {
   interface Window {  
-	LOOPINDEX: {
-		$: JQueryStatic;
-		FLITE: IFLITEGlobals;
-		LANCE: ILanceGlobals;				
-	};
+	LOOPINDEX: ILoopIndexGlobals;
   }
 }
 
