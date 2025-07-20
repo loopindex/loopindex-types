7c19beeccaf7e2fcdb3d49b7e2625a352cf92747
dfl <dfl@loopindex.com>
2025-07-20 09:50:07 +0300

    Better global namespace definition

--- global.d.ts
+++ global.d.ts
@@ -2,11 +2,14 @@ import { ILoopIndexGlobals } from ".";
 import type { IFLITEGlobals } from "./flite";
 import type { ILanceGlobals } from "./lance";
 
-declare global {
-  interface Window {  
-	  readonly LOOPINDEX: ILoopIndexGlobals;
+interface LIGlobals {
     readonly FLITE: IFLITEGlobals;
     readonly LANCE: ILanceGlobals;				
+}
+
+declare global {
+  interface Window {  
+	  readonly LOOPINDEX: ILoopIndexGlobals & LIGlobals;
   }
 }
 
