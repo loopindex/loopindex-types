7f952dff58cceec16e7c9915d063deb86ed9ecbe
dfl <dfl@loopindex.com>
2025-08-25 02:16:12 +0300

    Minor

--- global.d.ts
+++ global.d.ts
@@ -2,11 +2,6 @@ import { ILoopIndexGlobals } from ".";
 import type { IFLITEGlobals } from "./flite";
 import type { ILanceGlobals } from "./lance";
 
-interface LIGlobals {
-    readonly FLITE: IFLITEGlobals;
-    readonly LANCE: ILanceGlobals;				
-}
-
 export interface LoopIndexPluginGlobals extends ILoopIndexGlobals {
     readonly FLITE: IFLITEGlobals;
     readonly LANCE: ILanceGlobals;				
