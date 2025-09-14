fdb0f6a3c404e5555f870721b4a9c2a94b0796c3
dfl <dfl@loopindex.com>
2025-09-14 03:11:58 +0300

    logged post install

--- hooks/post-install.js
+++ hooks/post-install.js
@@ -10,21 +10,22 @@ else {
 	fs.writeFileSync("postinstall1.log", `starting post install at ${(new Date()).getMinutes()}:${(new Date()).getSeconds()}`);
 	const output = [];
 	const errors = [];
-	const proc = - p.spawn("bash", ["npm install"], {
+	const proc = - p.exec("npm install", {
 		env: {
 			LT_POST_HOOK: "yes"
 		}
 	});
+	fs.writeFileSync("process-result.log", `Child process: ${proc}/${typeof proc}, on: ${typeof proc?.on} members: ${Object.keys(proc ?? { unknown: "" })}`)
 	proc.stdout?.on('data', data => {
 		output.push(String(data));
 	});
 	proc.stderr?.on("data", function (data) {
 		errors.push(String(data));
 	});
-	proc.on("error", err => {
+	proc.on?.("error", err => {
 		fs.writeFileSync("postinstall-error.log", `ended post install with error ${err}`);
 	})
-	proc.on("close", function () {
+	proc.on?.("close", function () {
 		fs.writeFileSync("postinstall2.log", `ended post install at ${(new Date()).getMinutes()}:${(new Date()).getSeconds()}
 output: ${output.join('')}\nErrors: ${errors.join('')}`);
 	})
