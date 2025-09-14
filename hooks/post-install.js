490502f60859d5258047dbe1360b14be61e8faf9
dfl <dfl@loopindex.com>
2025-09-14 04:02:25 +0300

    final postinstall

--- hooks/post-install.js
+++ hooks/post-install.js
@@ -1,3 +1,8 @@
+//////////////////////////////////////////////////////////////
+// Post instal script ////////////////////////////////////////
+// designed to run `npm install` in the module folder ////////
+//////////////////////////////////////////////////////////////
+
 const p = require("node:child_process");
 const fs = require("node:fs");
 
@@ -14,13 +19,14 @@ const logIt = (name, msg) => {
 
 const guard = process.env.LT_POST_HOOK;
 
+// Don't Run npm install if we're in a process created by this script
 if (guard) {
 	logIt("postinstall-inner.log", `Not running inner call to postinstall`);
 }
 
 
 else {
-	logIt("postinstall1.log", `starting post install in ${process.cwd()}`);
+	logIt("preinstall.log", `starting post install in ${process.cwd()}`);
 	const output = [];
 	const errors = [];
 	const proc = - p.exec("npm install", {
@@ -28,18 +34,18 @@ else {
 			LT_POST_HOOK: "yes"
 		}
 	});
-	logIt("process-result.log", `Child process: ${proc}/${typeof proc}, on: ${typeof proc?.on} members: ${Object.keys(proc ?? { unknown: "" })}`)
-	proc.stdout?.on('data', data => {
+	// when running as a lifecycle script, proc is NaN
+	proc?.stdout?.on('data', data => {
 		output.push(String(data));
 	});
-	proc.stderr?.on("data", function (data) {
+	proc?.stderr?.on("data", function (data) {
 		errors.push(String(data));
 	});
-	proc.on?.("error", err => {
+	proc?.on?.("error", err => {
 		logIt("postinstall-error.log", `ended post install with error ${err}`);
 	})
-	proc.on?.("close", function () {
-		logIt("postinstall2.log", `ended post install
+	proc?.on?.("close", function () {
+		logIt("postinstall.log", `ended post install
 \toutput: ${output.join('')}
 \tErrors: ${errors.join('')}`);
 	})
